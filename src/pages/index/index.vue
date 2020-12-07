0ebb55f75d51f2dddf99b9b94ab95024cb4bd740
Ray <278227578@qq.com>
2020-12-07 19:40:05 +0800

    add demo

--- src/pages/index/index.vue
+++ src/pages/index/index.vue
@@ -4,21 +4,42 @@
 		<view>
 			<text class="title">{{title}}</text>
 		</view>
+		<block v-for="(item,index) in list" :key="index">
+			<view>{{item.name}}</view>
+		</block>
+		<view>
+			<text class="f-test-color">测试颜色</text>
+		</view>
+		<view class="f-container">
+			<view class="f-row-2 f-text-center" @tap="goto" :data-id="link1">链接一</view>
+			<view class="f-row-2 f-text-center" @tap="goto" :data-id="link2">链接二</view>
+		</view>
 	</view>
 </template>
 
 <script>
+	const _c = n=>({
+		name:n
+	});
 	export default {
 		data() {
 			return {
-				title: 'Hello'
+				title: 'Hello',
+				list:[1,2].map(v=>_c(v)),
+				link1:'1',
+				link2:'2',
 			}
 		},
 		onLoad() {
-
+			console.log('page onload');
 		},
 		methods: {
-
+			goto(e){
+				const {id} = e.currentTarget.dataset;
+				uni.navigateTo({
+					url:`../list/list?id=${id}`
+				});
+			}
 		}
 	}
 </script>
---
+++ src/pages/list/list.vue
@@ -0,0 +1,23 @@
+<template>
+  <view>
+      <text class="f-test-color">{{test}}</text>
+  </view>
+</template>
+
+<script>
+export default {
+    data(){
+        return {
+            test:'This is test content'
+        }
+    },
+    onLoad(options){
+        const {id} = options;
+        console.log(`Param is ${id}`);
+    }
+}
+</script>
+
+<style>
+
+</style>
